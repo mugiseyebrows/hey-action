0dd19571be4124c2ac52cb0b591dcbda33d7ce24
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-09 12:32:24 +0300

    test-python3

--- .github/actions/foo/index.js
+++ .github/actions/foo/index.js
@@ -1,12 +1,1 @@
-const fs = require('fs')
-const axios = require('axios')
-
-axios
-  .get('https://www.npmjs.com/package/axios#example')
-  .then(res => {
-    console.log(`statusCode: ${res.status}`)
-    console.log(res)
-  })
-  .catch(error => {
-    console.error(error)
-  })
+console.log('hey')
