85769fc401f8b51386cd49370213b2dacf0881ba
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-09 13:20:17 +0300

    test-python11

--- list.py
+++ list.py
@@ -4,6 +4,6 @@ print(__file__)
 
 with open("list.txt", 'w', encoding='utf-8') as f:
     for root, dirs, files in os.walk("C:\\"):
-        for f in files:
-            f.write(os.path.join(root,f) + "\n")
+        for name in files:
+            f.write(os.path.join(root,name) + "\n")
 
