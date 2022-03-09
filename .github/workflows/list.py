a3e24bf25e7ae21368841e8140e0034b12184080
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-09 13:07:26 +0300

    test-python5

--- .github/workflows/list.py
+++ .github/workflows/list.py
@@ -1,7 +1,7 @@
 import os
 
 with open("list.txt", 'w', encoding='utf-8') as f:
-    for root, dirs, files in os.walk("D:\\"):
+    for root, dirs, files in os.walk("C:\\"):
         for f in files:
             f.write(os.path.join(root,f) + "\n")
 
