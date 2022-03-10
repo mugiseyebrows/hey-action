7f8fe36787f4e973a10db30a05b33889b64e0239
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-10 14:20:46 +0300

    test-conda6

--- .github/workflows/create-env.bat
+++ .github/workflows/create-env.bat
@@ -1,5 +1,6 @@
 set PATH=C:\Miniconda;C:\Miniconda\Scripts;C:\Miniconda3\Library\bin;%PATH%
 conda create -n aqtenv -y
-call conda.bat activate aqtenv
+conda init cmd.exe
+conda activate aqtenv
 pip install aqtinstall
 conda info --envs
