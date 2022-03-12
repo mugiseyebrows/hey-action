f81e8efef671bc49da779671c15396c111e51d2a
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-12 13:19:29 +0300

    build-base2

--- build.bat
+++ build.bat
@@ -24,8 +24,8 @@ exit /b
 pushd %~dp0
 if not exist "x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z" "%CURL%" -L -o "x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z" https://storage.googleapis.com/qt-binaries/x86_64-8.1.0-release-posix-seh-rt_v6-rev0.7z
 if not exist "qtbase-everywhere-src-5.15.2.zip" "%CURL%" -L -o "qtbase-everywhere-src-5.15.2.zip" https://download.qt.io/official_releases/qt/5.15/5.15.2/submodules/qtbase-everywhere-src-5.15.2.zip
-xcopy /e /i "C:\Program Files\PostgreSQL\14\bin" pgbin
-xcopy /e /i "C:\Program Files\PostgreSQL\14\include" pginclude
+xcopy /e /i "C:\Program Files\PostgreSQL\14\bin" pgbin > NUL
+xcopy /e /i "C:\Program Files\PostgreSQL\14\include" pginclude > NUL
 popd
 
 pushd %~dp0
@@ -37,12 +37,10 @@ pushd %~dp0
 if "%SKIP_BASE%" neq "1" (
 pushd qtbase-everywhere-src-5.15.2
 del /f config.cache
-call configure -prefix %~dp0Qt-5.15.2-mingw64 -opensource -confirm-license -shared -plugin-sql-odbc -plugin-sql-psql -plugin-sql-mysql -platform win32-g++ -opengl desktop -release -nomake tests MYSQL_PREFIX=C:\mysql MYSQL_INCDIR=C:\mysql\include MYSQL_LIBDIR=C:\mysql\lib PSQL_INCDIR=%~dp0pgnclude PSQL_LIBDIR=%~dp0pgbin
-mingw32-make -j4
-mingw32-make install
+call configure -prefix %~dp0Qt-5.15.2-mingw64 -opensource -confirm-license -shared -plugin-sql-odbc -plugin-sql-psql -plugin-sql-mysql -platform win32-g++ -opengl desktop -release -nomake tests -nomake examples MYSQL_PREFIX=C:\mysql MYSQL_INCDIR=C:\mysql\include MYSQL_LIBDIR=C:\mysql\lib PSQL_INCDIR=%~dp0pgnclude PSQL_LIBDIR=%~dp0pgbin
+type qtbase-everywhere-src-5.15.2\config.log
 popd
 )
-del /f Qt-5.15.2-mingw64.zip
 if not exist "Qt-5.15.2-mingw64.zip" 7z a -y "Qt-5.15.2-mingw64.zip" "Qt-5.15.2-mingw64"
 popd
 
