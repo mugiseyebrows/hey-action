734691b4c2da19fd7dced01d2c2ecc51cc9fbef0
Doronin Stanislav <mugisbrows@gmail.com>
2022-06-07 14:19:31 +0300

    fix python cache

--- testqstring.cpp
+++ testqstring.cpp
@@ -1,7 +1,13 @@
 #include "testqstring.h"
 
+#include <QDebug>
+
 void TestQString::toUpper()
 {
+  qDebug() << "hello from debug";
+
+  printf("hello from printf\n");
+
   QString str = "Hello";
   QVERIFY(str.toUpper() == "HELLO");
 }
