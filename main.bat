0214b0ee0e23516bc4bcdc1a4c912dd8bc536301
Doronin Stanislav <mugisbrows@gmail.com>
2022-12-09 22:46:02 +0300

    windows-2022-listing

--- main.bat
+++ main.bat
@@ -2,5 +2,7 @@
 rem This file is generated from main.pbat, all edits will be lost
 set PATH=C:\Miniconda3;C:\Miniconda3\Scripts;%PATH%
 python -m pip install mugicli
+pyfind -maxdepth 2 C:\ > c.txt
+pyfind "C:\Program Files" "C:\Program Files (x86)" > program-files.txt
 
 
